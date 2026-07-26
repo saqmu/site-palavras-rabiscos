f4e6fe2e9b6b5854eb441f44e9e8bc075d2827b8
saqmu <samuelcostaortiz2025@gmail.com>
2026-07-26 18:03:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-const blob = document.getElementById('blob');
+const blob = document.getElementById("blob");
 
-document.body.onpointermove = event => {
+window.onpointermove = event => { 
     const { clientX, clientY } = event;
     
-    // Suavizando o movimento da bolha de luz
     blob.animate({
-        left: `${clientX - 150}px`,
-        top: `${clientY - 150}px`
+        left: `${clientX - 200}px`,
+        top: `${clientY - 200}px`
     }, { duration: 1500, fill: "forwards" });
 }
-
-// Logica simples de rolagem suave (scroll)
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
